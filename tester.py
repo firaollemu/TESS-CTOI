c4aaf637149f42beed01573c02b09a72d3cc3048
firaollemu <120162822+firaollemu@users.noreply.github.com>
2023-06-29 12:03:34 +0300

    ctoi_false_positive

--- tester.py
+++ tester.py
@@ -27,27 +27,31 @@ exofop_false_positive.append(exofop_df_headers)
 
 
 
-for index, row in exofop_df.iterrows():
-    if row['TFOPWG Disposition'] == 'FP':
-        exofop_row_values = [
-            int(row['TIC ID']),
-            row['CTOI'],
-            row['TFOPWG Disposition']
-        ]
+# for index, row in exofop_df.iterrows():
+#     if row['TFOPWG Disposition'] == 'FP':
+#         exofop_row_values = [
+#             int(row['TIC ID']),
+#             row['CTOI'],
+#             row['TFOPWG Disposition']
+#         ]
 
-        exofop_row_values.append('Nan')
-        exofop_row_values.append('Likely FP')
+#         exofop_row_values.append('Nan')
+#         exofop_row_values.append('Likely FP')
 
-        exofop_false_positive.append(exofop_row_values)
+#         exofop_false_positive.append(exofop_row_values)
 
 
 
 
 category1 = 'FP (EB)'
 category2 = 'FP(EB)'
+category3 = 'EB' 
+
+
+eclipsing_binary = ['FP (EB)', 'FP(EB)', 'EB']
 
 for index, row in ctoi_review_fp_df.iterrows():
-    if row['TEV Disposition'] == category1 or row['TEV Disposition'] == category2:
+    if (row['TEV Disposition'] == category1) or (row['TEV Disposition'] == category2) or (row['TEV Disposition'] == category3) :
         row_values = [
             int(row['TIC']),
             row['CTOI'],
@@ -55,6 +59,7 @@ for index, row in ctoi_review_fp_df.iterrows():
             row['Notes']
         ]
         row_values.append('Likely FP')
+        
         ctoi_review_false_positives.append(row_values)
 
 
@@ -78,18 +83,18 @@ exofop_df = pd.read_csv(exofop_fp_file)
 common_columns = ['TIC', 'CTOI', 'Notes', 'CTOI Category']
 
 # Rename the title of the column name in the EXOFOP false positive list
-exofop_df.rename(columns={'TIC ID': 'TIC'}, inplace=True)
+# exofop_df.rename(columns={'TIC ID': 'TIC'}, inplace=True)
 
 
 
 
 # Now, merge these two
-merged_df = pd.merge(ctoi_df, exofop_df, on=['TIC', 'CTOI', 'Notes', 'CTOI Category'], how='inner')
+# merged_df = pd.merge(ctoi_df, exofop_df, on=['TIC', 'CTOI', 'Notes', 'CTOI Category'], how='inner')
 
 
 
 # save the merged pandas dataframe into a csv file
-merged_df.to_csv('Mereged_false_positives.csv', index=False)
+# merged_df.to_csv('Mereged_false_positives.csv', index=False)
 
 # Merging error. 
 
@@ -98,9 +103,9 @@ with open(ctoi_file, 'w', newline='') as ctoi_fp_file:
     writer.writerows(ctoi_review_false_positives) 
 
 
-with open(exofop_fp_file, 'w', newline='') as exofop_fp:
-    writer = csv.writer(exofop_fp)
-    writer.writerows(exofop_false_positive)
+# with open(exofop_fp_file, 'w', newline='') as exofop_fp:
+#     writer = csv.writer(exofop_fp)
+#     writer.writerows(exofop_false_positive)
 
 
 
