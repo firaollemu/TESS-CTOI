6c2116eba35c885485644bb34ee496885843c05f
firaollemu <120162822+firaollemu@users.noreply.github.com>
2023-07-10 23:45:27 +0300

    Created a master csv file for the CTOIs that need more review.

--- need_review.py
+++ need_review.py
@@ -31,8 +31,8 @@ ctois_need_more_review_from_review_sheet = []
 ctois_need_more_review_from_review_sheet.append(ctoi_review_sheet_header)
 
 
-ctois_need_more_review_from_tois = []
-ctois_need_more_review_from_tois.append(exofop_header)
+ctois_need_more_review_from_exofop = []
+ctois_need_more_review_from_exofop.append(exofop_header)
 
 
 # loop through the ctoi_review_sheet and see if the TIC exists in either the ctoi_fp. If not add it to a list to be added to a new csv file.
@@ -60,35 +60,77 @@ for _, row in exofop_df.iterrows():
             row['Period (days)']
         ]
 
-        ctois_need_more_review_from_tois.append(row_values)
+        ctois_need_more_review_from_exofop.append(row_values)
 
 
 
 
 # Now merge this two lists.
-master_csv_header = ['TIC', 'CTOI', 'Period (exofop)', 'Period (TEV)', 'TESS mag', 'TFOPWG Disposition', 'Notes']
 
 # Period (days) in new_tois same thing as period (exofop) in ctoi_fp_sheet
 
 ctois_need_more_review_from_fp_sheet = 'need_more_review_from_fp_sheet.csv'
-ctois_need_more_review_from_TOI_csv = 'need_more_review_from_TOIs.csv'
+ctois_need_more_review_from_exofop_csv = 'need_more_review_from_TOIs.csv'
 
-with open(ctois_need_more_review_from_fp_sheet, 'w', newline='') as from_fp_sheet:
-    writer = csv.writer(from_fp_sheet)
-    writer.writerows(ctois_need_more_review_from_review_sheet)
-
-
-
-with open(ctois_need_more_review_from_TOI_csv, 'w', newline='') as from_TOIs:
-    writer = csv.writer(from_TOIs)
-    writer.writerows(ctois_need_more_review_from_tois)
+# with open(ctois_need_more_review_from_fp_sheet, 'w', newline='') as from_fp_sheet:
+#     writer = csv.writer(from_fp_sheet)
+#     writer.writerows(ctois_need_more_review_from_review_sheet)
 
 
 
+# with open(ctois_need_more_review_from_exofop_csv, 'w', newline='') as from_exofop:
+#     writer = csv.writer(from_exofop)
+#     writer.writerows(ctois_need_more_review_from_exofop)
+
+
+# NOW MERGE THE TWO CSV FILES CONTAINING CTOIS THAT NEED MORE REVIEW. 
+
+review_ctois_from_fp_sheet = pd.read_csv(ctois_need_more_review_from_fp_sheet)
+review_ctois_from_exofop = pd.read_csv(ctois_need_more_review_from_exofop_csv)
+
+
+
+master_csv_header = ['TIC', 'CTOI', 'Period (exofop)', 'Period (TEV)', 'Notes', 'TOI Comments']
+
+master_data = []
+
+
+for _, row in review_ctois_from_fp_sheet.iterrows():
+    master_row = {}
+
+    for column in master_csv_header:
+        if column in ctoi_review_sheet_header:
+            master_row[column] = row[column]
+        else:
+            master_row[column] = ""
+        
+    
+    master_data.append(master_row)
+
+
+for _, row in review_ctois_from_exofop.iterrows():
+    master_row = {}
+
+    for column in master_csv_header:
+        if column in exofop_header:
+            master_row[column] = row[column]
+        else:
+            master_row[column] = ""
+        
+    
+    master_data.append(master_row)
 
 
 
 
 
+master_df = pd.DataFrame(master_data, columns=master_csv_header)
 
 
+additional_column_name = 'TOI Comments'
+master_df[additional_column_name] = master_df[additional_column_name].fillna('In review')
+
+
+
+
+master_df.to_csv('need_review1.csv', index=False)
